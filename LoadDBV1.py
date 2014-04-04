7cb206cddce08e345cb8879a9166bbe708ae9af2
Grant DeLozier <grantdelozier@gmail.com>
2014-04-04 02:01:27 -0500

    Added LoadDBV1.py ; it takes in a train file and creates a table with geometry corresponding to the documents in trainfile - Grant

---
+++ LoadDBV1.py
@@ -0,0 +1,87 @@
+import psycopg2
+import datetime
+import io
+
+class Document:
+
+    userID = ""
+    userLat = ""
+    userLong = ""
+
+    Feature_Freq = {}
+    total_words = 0
+    #Feature_Prob = {}
+
+    def __init__(self, ID, latit, longit, file_from):
+        self.userID = ID
+        self.userLat = latit
+        self.userLong = longit
+        self.Feature_Freq = F_Freq
+        self.fileFrom = file_from
+        
+    
+
+def Load(tf, tbl_name, conn_info):
+
+    #Connecting to Database
+    conn = psycopg2.connect(conn_info)
+    print "DB Connection Success"
+
+    trainFile = tf
+    
+    #openTrain = codecs.open(trainFile, 'r', encoding='utf-8')
+    filename = trainFile[trainFile.rfind('/')+1:]
+    print filename
+    begin_time = datetime.datetime.now()
+    docList = []
+    x = 0
+    y = 0
+
+    read_time_begin = datetime.datetime.now()
+    z = 0
+    
+    with io.open(trainFile, 'r', encoding='utf-8') as f:
+        for person in f:
+            x += 1
+            #print "####NEW Doucment####"
+            #print userID, latit, longit
+            try:
+                row = person.strip().split('\t')
+                #print row[0]
+                userID = row[0]
+                latit = row[1].split(',')[0]
+                longit = row[1].split(',')[1]
+
+                newDocument = Document(userID, latit, longit, filename)
+                docList.append(newDocument)
+            except:
+                print "@@@@@error reading user@@@@@@"
+                print row
+                print z
+                #break
+            z += 1
+            if z % 5000 == 0:
+                print z
+                print datetime.datetime.now()
+
+    print "------Done reading in the data-------"
+    read_time_end = datetime.datetime.now()
+    print read_time_end - read_time_begin
+
+        cur = conn.cursor()
+
+    cur.execute("CREATE TABLE IF NOT EXISTS %s (uid varchar(20) primary key, latit float, longit float, coord geometry(Point, 4326), filefrom varchar(50));" % (tbl_name, ))
+
+    cur.execute("DELETE FROM %s" % tbl_name)
+   
+    for p in docList:
+        SQL1 = "INSERT INTO %s VALUES (%s, %s, %s, ST_GeomFromText('POINT(%s %s)', 4326), %s);" % (tbl_name, '%s', '%s', '%s', '%s', '%s', '%s')
+        data = (p.userID, float(p.userLat), float(p.userLong), float(p.userLong), float(p.userLat), p.fileFrom)
+        cur.execute(SQL1, data)
+
+    conn.commit()
+    conn.close()
+
+    print "Number of people loaded: ", len(docList)
+
+    print "Done Loading ", tbl_name
--- TopoCluster.py
+++ TopoCluster.py
@@ -6,9 +6,32 @@ if len(sys.argv) >= 3:
 
     mode_arg = args[args.index("-mode")+1]
     print mode_arg
+
+    ##########################Load a database with | Doc ID | Geometry | table#####################
     if mode_arg.lower() == "loaddb":
-        import LoadDBV1
+        import LoadDBV1 as loadDB
         print "Starting DB Load Process"
+
+        if '-tf' in args:
+            f = args[args.index("-tf")+1]
+        elif '-df' in args:
+            f = args[args.index("-df")+1]
+        elif '-tstf' in args:
+            f = args[args.index("-tstf")+1]
+            
+        try:
+            ptbl = args[args.index("-dtbl")+1]
+        except:
+            print "You did not provide a name for a document table to output"
+            sys.exit("Error")
+            
+
+        try:
+            conn = args[args.index('-conn')+1]
+        except:
+            print "Problem parsing the connection information provided"
+            sys.exit("Error")
+
 
     if "-help" in args:
         print "TopoCluster Run Modes:"
