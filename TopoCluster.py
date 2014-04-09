205db23b71540896f4298b7f011675a68e66600a
Grant DeLozier <grantdelozier@gmail.com>
2014-04-09 00:00:47 -0500

    LoadDB mode now works. Added a new calc_morans mode (under development). Fixed a few bugs related to variable names.

--- TopoCluster.py
+++ TopoCluster.py
@@ -20,7 +20,13 @@ if len(sys.argv) >= 3:
             f = args[args.index("-tstf")+1]
             
         try:
-            ptbl = args[args.index("-dtbl")+1]
+            traintype = args[args.index("-traintype")+1]
+        except:
+            print "You did not provide a training file type (wiki or twitter)"
+            sys.exit("Error")
+
+        try:
+            dtbl = args[args.index("-dtbl")+1]
         except:
             print "You did not provide a name for a document table to output"
             sys.exit("Error")
@@ -32,11 +38,53 @@ if len(sys.argv) >= 3:
             print "Problem parsing the connection information provided"
             sys.exit("Error")
 
+        loadDB.Load(f, dtbl, conn, traintype.lower())
+
+    if mode_arg.lower() == "calc_morans":
+        import MoransV1 as morans
+        print "Starting Morans Coef. Calculation"
+
+        if '-tf' in args:
+            f = args[args.index("-tf")+1]
+        elif '-df' in args:
+            f = args[args.index("-df")+1]
+        elif '-tstf' in args:
+            f = args[args.index("-tstf")+1]
+
+
+        try:
+            ptbl = args[args.index("-dtbl")+1]
+        except:
+            print "You did not provide a name for the input document table"
+            sys.exit("Error")
+
+        try:
+            gtbl = args[args.index('-gtbl')+1]
+        except:
+            print "You did not provide a grid table argument"
+            sys.exit("Error")         
+
+        try:
+            conn = args[args.index('-conn')+1]
+        except:
+            print "Problem parsing the connection information provided"
+            sys.exit("Error")
+
+        try:
+            outf = args[args.index('-outf')+1]
+        except:
+            print "You did not provide an outfile for where the moran's scores will be written"
+            sys.exit("Error")
+
+        
+
+        morans.calc(f, dtbl, gtbl, conn, outf)
+
 
     if "-help" in args:
         print "TopoCluster Run Modes:"
         print "-mode loadDB"
-        print "-mode morans"
+        print "-mode calc_morans"
         print ""
         print "======================"
         print "=======Example========"
