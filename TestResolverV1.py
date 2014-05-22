3f459b6298543d52298e12b670d3bc5edfd24209
Grant DeLozier <grantdelozier@gmail.com>
2014-05-22 11:13:16 -0500

    Adding new gazetteer matching criteria to TestResolverV1

--- TestResolverV1.py
+++ TestResolverV1.py
@@ -362,74 +362,64 @@ def GetGazets(cur, placenames, latlong, country_tbl, region_tbl, state_tbl, US_P
 		return [ranked_gazet[0]]
 	return gazet_entry
 
+def GetGazets_DistLimited(cur, placenames, latlong, country_tbl, region_tbl, state_tbl, US_Prominent_tbl, Wrld_Prominent_tbl):
+	names = tuple(x for x in placenames)
+	DistLimit = 300.0
+	print names
+	gazet_entry = []
+	ranked_gazet = []
+	SQL1 = "SELECT p1.gid, p1.name, ST_Distance(p1.geog, ST_GeographyFromText('SRID=4326;POINT(%s %s)')) FROM %s as p1 WHERE p1.name IN %s or p1.fips_cntry IN %s or p1.gmi_cntry IN %s or p1.locshrtnam IN %s;" % (latlong[1], latlong[0], country_tbl, '%s', '%s', '%s', '%s')
+	SQL2 = "SELECT p2.gid, p2.name, ST_Distance(p2.geog, ST_GeographyFromText('SRID=4326;POINT(%s %s)')) FROM %s as p2 WHERE p2.name in %s;" % (latlong[1], latlong[0], region_tbl, '%s')
+	SQL3 = "SELECT p3.gid, p3.name, ST_Distance(p3.geog, ST_GeographyFromText('SRID=4326;POINT(%s %s)')) FROM %s as p3 WHERE p3.name in %s;" % (latlong[1], latlong[0], state_tbl, '%s')
+	SQL4 = "SELECT p4.gid, p4.name, ST_Distance(p4.geog, ST_GeographyFromText('SRID=4326;POINT(%s %s)')) FROM %s as p4 WHERE p4.name in %s;" % (latlong[1], latlong[0], US_Prominent_tbl, '%s')
+	SQL5 = "SELECT p5.gid, p5.name, ST_Distance(p5.geog, ST_GeographyFromText('SRID=4326;POINT(%s %s)')) FROM %s as p5 WHERE p5.name in %s;" % (latlong[1], latlong[0], Wrld_Prominent_tbl, '%s')
+	cur.execute(SQL1, (names, names, names, names))
+	returns = cur.fetchall()
+	for row in returns:
+                dist = float(row[2])/1000.0
+                if dist < DistLimit:
+                        gazet_entry.append([country_tbl, row[0], row[1], dist])
+                        #print "!!!Found Gazet Match!!!"
+                        #print gazet_entry[-1]
+	cur.execute(SQL2, (names, ))
+	returns = cur.fetchall()
+	for row in returns:
+                dist = float(row[2])/1000.0
+                if dist < DistLimit:
+                        gazet_entry.append([region_tbl, row[0], row[1], float(row[2])/1000.0])
+                        #print "!!!Found Gazet Match!!!"
+                        #print gazet_entry[-1]
+	cur.execute(SQL3, (names, ))
+	returns = cur.fetchall()
+	for row in returns:
+                dist = float(row[2])/1000.0
+                if dist < DistLimit:
+                        gazet_entry.append([state_tbl, row[0], row[1], float(row[2])/1000.0])
+                        #print "!!!Found Gazet Match!!!"
+                        #print gazet_entry[-1]
+	cur.execute(SQL4, (names, ))
+	returns = cur.fetchall()
+	for row in returns:
+                dist = float(row[2])/1000.0
+                if dist < DistLimit:
+                        gazet_entry.append([US_Prominent_tbl, row[0], row[1], float(row[2])/1000.0])
+                        #print "!!!Found Gazet Match!!!"
+                        #print gazet_entry[-1]
+	cur.execute(SQL5, (names, ))
+	returns = cur.fetchall()
+	for row in returns:
+                dist = float(row[2])/1000.0
+                if dist < DistLimit:
+                        gazet_entry.append([Wrld_Prominent_tbl, row[0], row[1], float(row[2])/1000.0])
+                        #print "!!!Found Gazet Match!!!"
+                        #print gazet_entry[-1]
+	if len(gazet_entry) > 1:
+		ranked_gazet = sorted(gazet_entry, key=itemgetter(3), reverse=False)
+		return [ranked_gazet[0]]
+	return gazet_entry
 
-def MostOverlap(wordref, toporef, point_error, cur, lat_long_lookup, stat_tbl, percentile, window, stopwords, place_name_weight, xml):
-	for j in toporef:
-		print "========Most Overlap============="
-		gold_lat = float(toporef[j][1]['lat'])
-		gold_long = float(toporef[j][1]['long'])
-		topotokens = [toporef[j][0]]
-		if "." in toporef[j][0]:
-			for token in toporef[j][0].split("."):
-				topotokens.append(token)
-		if " " in toporef[j][0]:
-			for token in toporef[j][0].split(" "):
-				topotokens.append(token)
-		contextlist = getContext(wordref, j, window, stopwords)
-		if toporef[j][0].title() != toporef[j][0]:
-			contextlist.append(toporef[j][0].title())
-			topotokens.append(toporef[j][0].title())
-		'''if toporef[j][0].lower() != toporef[j][0]:
-			contextlist.append(toporef[j][0].lower())
-			topotokens.append(toporef[j][0].lower())
-		if toporef[j][0].upper() != toporef[j][0]:
-			contextlist.append(toporef[j][0].upper())
-			topotokens.append(toporef[j][0].upper())'''
-		totaldict = Counter()
-		print contextlist
-		contrib_dict = {}
-		print toporef[j][0], float(toporef[j][1]['lat']), float(toporef[j][1]['long'])
-		for word in contextlist:
-			if word not in stopwords:
-				SQL = "Select gid, stat FROM %s WHERE word = %s ;" % (stat_tbl, '%s')
-				cur.execute(SQL, (word, ))
-				if word in topotokens:
-					weight = place_name_weight
-				else: weight = 1.0
-				adict =  dict([(int(k), weight * float(v)) for k, v in cur.fetchall()])
-				ranked_fetch = sorted(adict.items(), key=itemgetter(1), reverse=True)
-				subset_ranked = dict([(row[0], weight * 1.0) for row in ranked_fetch[:int(len(ranked_fetch)*percentile)]])
-				for gid in subset_ranked:
-					#print gid
-					contrib_dict.setdefault(gid, list()).append([word, subset_ranked[gid]])
-					#contrib_dict[gid] = combine_tuples(contrib_dict.get(gid, (word, 0.0)), gid)
-				#print word, ranked_fetch[:5]
-				totaldict += Counter(subset_ranked)
-		#print totaldict
-		sorted_total = sorted(totaldict.items(), key=itemgetter(1), reverse=True)
-		y = 0
-		rank_dict = {}
-		#ranked_contrib = sorted(contrib_dict.items(), key=itemgetter(1), reverse=True)
-		for t in sorted_total:
-			y += 1 
-			contrib_sub = sorted(contrib_dict[t[0]], key=itemgetter(1), reverse=True)
-			rank_dict[t[0]] = [y, t[1], lat_long_lookup[t[0]], math.fabs(lat_long_lookup[t[0]][0]-gold_lat)+math.fabs(lat_long_lookup[t[0]][1]-gold_long), contrib_sub[:5]]
 
-		#print sorted_total[:20]
-		y = 0
-		for i in sorted_total[:5]:
-			y += 1
-			if y == 1:
-				print rank_dict[i[0]]
-				SQL_Dist = "SELECT ST_Distance(p1.pointgeog, ST_GeographyFromText('SRID=4326;POINT(%s %s)')) FROM trconllf as p1 WHERE p1.placename = %s and p1.docname = %s;" % (rank_dict[i[0]][2][1], rank_dict[i[0]][2][0], '%s', '%s')
-				cur.execute(SQL_Dist, (toporef[j][0], xml))
-				results = cur.fetchall()
-				pointdist = results[0][0]
-				print pointdist / float(1000)
-				point_error += (pointdist / float(1000))
-		print "====================="
-		#print len(contextlist)
-	return point_error
+
 
 def combine_tuples(t1, t2):
 	tsum = t1[1] + t2[1]
