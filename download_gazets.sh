449007d803b7bcdb729922c2ea2aa21217d4446d
Grant <grantdelozier@gmail.com>
2016-11-25 20:26:49 -0600

    Updated download script to new location

--- download_gazets.sh
+++ download_gazets.sh
@@ -1,11 +1,11 @@
 #!/bin/sh
 
-wget http://web.corral.tacc.utexas.edu/utcompling/topocluster-data/table_backups/enwiki20130102_ner_final_atoi.backup -O data/enwiki20130102_ner_final_atoi.backup
-wget http://web.corral.tacc.utexas.edu/utcompling/topocluster-data/table_backups/enwiki20130102_ner_final_jtos.backup -O data/enwiki20130102_ner_final_jtos.backup
-wget http://web.corral.tacc.utexas.edu/utcompling/topocluster-data/table_backups/enwiki20130102_ner_final_ttoz.backup -O data/enwiki20130102_ner_final_ttoz.backup
-wget http://web.corral.tacc.utexas.edu/utcompling/topocluster-data/table_backups/enwiki20130102_ner_final_other.backup -O data/enwiki20130102_ner_final_other.backup
+wget https://s3.amazonaws.com/intellilease-docs/topocluster/enwiki20130102_ner_final_atoi.backup -O data/enwiki20130102_ner_final_atoi.backup
+wget https://s3.amazonaws.com/intellilease-docs/topocluster/enwiki20130102_ner_final_jtos.backup -O data/enwiki20130102_ner_final_jtos.backup
+wget https://s3.amazonaws.com/intellilease-docs/topocluster/enwiki20130102_ner_final_ttoz.backup -O data/enwiki20130102_ner_final_ttoz.backup
+wget https://s3.amazonaws.com/intellilease-docs/topocluster/enwiki20130102_ner_final_other.backup -O data/enwiki20130102_ner_final_other.backup
 
-wget http://web.corral.tacc.utexas.edu/utcompling/topocluster-data/table_backups/geonames_all.backup -O data/geonames_all.backup
-wget http://web.corral.tacc.utexas.edu/utcompling/topocluster-data/table_backups/countries_2012.backup -O data/countries_2012.backup
-wget http://web.corral.tacc.utexas.edu/utcompling/topocluster-data/table_backups/regions_2012.backup -O data/regions_2012.backup
-wget http://web.corral.tacc.utexas.edu/utcompling/topocluster-data/table_backups/states_2012.backup -O data/states_2012.backup
+wget https://s3.amazonaws.com/intellilease-docs/topocluster/geonames_all.backup -O data/geonames_all.backup
+wget https://s3.amazonaws.com/intellilease-docs/topocluster/countries_2012.backup -O data/countries_2012.backup
+wget https://s3.amazonaws.com/intellilease-docs/topocluster/regions_2012.backup -O data/regions_2012.backup
+wget https://s3.amazonaws.com/intellilease-docs/topocluster/states_2012.backup -O data/states_2012.backup
